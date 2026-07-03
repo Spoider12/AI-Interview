e2b27abf2ec247bb0895f8c8b46a5cdaceacc057
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-07-03 19:57:15 +0530

    register and login ui implemented

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../auth/auth.form.scss'
+import { useNavigate, Link } from 'react-router'
 
 const Login = () => {
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  }
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="input-group">
                 <label htmlFor="email">Email</label>
                 <input type="email" id="email" name="email" placeholder="Enter your Email" />
@@ -15,8 +20,9 @@ const Login = () => {
                 <label htmlFor="password">Password</label>
                 <input type="password" id="password" name="password" placeholder="Enter your Password" />
             </div>
-            <button type="submit">Login</button>
+            <button className='button primary-button' type="submit">Login</button>
         </form>
+        <p>Don't have an account? <Link to={"/register"} >Register</Link> </p>
         </div>  
     </main>
   )
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import {useNavigate, Link} from 'react-router'
 const Register = () => {
+    const handleSubmit = (e) =>{
+    e.preventDefault();
+    }
   return (
     <div>
       <main>
         <div className="form-container">
           <h1>Register</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="input-group">
               <label htmlFor="username">Username</label>
               <input type="text" id="username" name="username" placeholder="Enter your Username" />
@@ -19,8 +22,9 @@ const Register = () => {
               <label htmlFor="password">Password</label>
               <input type="password" id="password" name="password" placeholder="Type your Password" />
             </div>
-            <button type="submit">Register</button>
+            <button className='button primary-button' type="submit">Register</button>
           </form>
+          <p> Already Have an Account <Link to={"/login"} >Login</Link> </p>
         </div>
       </main>
     </div>
