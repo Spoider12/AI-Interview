34fd4f7242f8e2727b5e586017ead40457d7e42e
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-07-03 13:10:14 +0530

    Login & Register form implemented

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import '../auth/auth.form.scss'
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form>
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your Email" />
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your Password" />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        </div>  
+    </main>
   )
 }
 
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
-      Register
+      <main>
+        <div className="form-container">
+          <h1>Register</h1>
+          <form>
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" name="username" placeholder="Enter your Username" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" placeholder="Enter your Email" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" placeholder="Type your Password" />
+            </div>
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
