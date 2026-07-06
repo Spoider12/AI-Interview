09dfff407b7e4420bacdac5d9ffd18e41bb93230
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-07-06 20:27:41 +0530

    kuch to kara hai bhai

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React , {useState} from 'react'
 import {useNavigate, Link} from 'react-router'
+import { useAuth } from '../hooks/useAuth'
 const Register = () => {
-    const handleSubmit = (e) =>{
+  const navigate  = useNavigate()
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState(" ")
+  const {loading, handleRegister} = useAuth()
+    const handleSubmit =  async(e) =>{
     e.preventDefault();
+    await handleRegister({username,email,password})
+    navigate("/")
     }
+    if(loading){
+    return (<main><h1>Loading......</h1></main>)
+  }
   return (
     <div>
       <main>
@@ -12,15 +23,21 @@ const Register = () => {
           <form onSubmit={handleSubmit}>
             <div className="input-group">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" name="username" placeholder="Enter your Username" />
+              <input
+              onChange={(e) => {setUsername(e.target.value)}}
+               type="text" id="username" name="username" placeholder="Enter your Username" />
             </div>
             <div className="input-group">
               <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" placeholder="Enter your Email" />
+              <input
+              onChange={(e) => {setEmail(e.target.value)}}
+               type="email" id="email" name="email" placeholder="Enter your Email" />
             </div>
             <div className="input-group">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" name="password" placeholder="Type your Password" />
+              <input
+              onChange={(e) => {setPassword(e.target.value)}}
+               type="password" id="password" name="password" placeholder="Type your Password" />
             </div>
             <button className='button primary-button' type="submit">Register</button>
           </form>
