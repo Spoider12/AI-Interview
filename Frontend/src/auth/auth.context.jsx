1060b7684c6cb83613a5900b1fcca840a8cf5f46
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-07-08 08:53:25 +0530

    Hooks Modified

--- Frontend/src/auth/auth.context.jsx
+++ Frontend/src/auth/auth.context.jsx
@@ -1,5 +1,5 @@
-import { createContext,useState } from "react";
-
+import { createContext,useState,useEffect } from "react";
+import { getMe } from "../services/auth.api";
 
 export const AuthContext = createContext()
 
@@ -8,7 +8,7 @@ export const AuthProvider = ({children}) =>{
     const [user , setUser] = useState(null)
     const [loading, setLoading] = useState(false)
 
-
+   
     return (
         <AuthContext.Provider value ={{user,setUser,loading,setLoading}} >
             {children}
