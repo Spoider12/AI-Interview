f3dddd39b2bdb2050303d2de998921a86a53145d
Spoider12 <153114166+Spoider12@users.noreply.github.com>
2026-07-12 14:52:12 +0530

    updated home page

--- Frontend/src/interview/pages/Home.jsx
+++ Frontend/src/interview/pages/Home.jsx
@@ -4,20 +4,23 @@ import "../style/home.scss"
 const Home = () => {
   return (
     <main className='home'>
-        <div className='left'>
-            <textarea name='jobDescription' id='jobDescription' placeholder='Enter your job Description here'></textarea>
-        </div>
-        <div className="right">
-            <div className="input-group">
-                <label htmlFor='resume'> Upload your Resume</label>
-                <input type='file' name='resume' id='resume' accept='pdf'/>
+        <div className="interview-input-group">
+            <div className="left">
+                <textarea name='jobDescription' id='jobDescription' placeholder='Enter your Job Description here...'></textarea>
             </div>
-            <div className="input-group">
-                <label htmlFor='selfDescription'>Slef Description</label>
-                <textarea name='selfDescription' id='selfDescription' placeholder='Describe yourself in few Sentences...'></textarea>
+            <div className="right">
+                <div className="input-group">
+                    <label htmlFor='resume'>Upload your Resume</label>
+                    <input type='file' name='resume' id='resume' accept='.pdf'/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor='selfDescription'>Self Description</label>
+                    <textarea name='selfDescription' id='sefDescription' placeholder='Enter your Self Description here'></textarea>
+                </div>
+                <button className='generate-btn'>Generate Interview Report</button>
             </div>
-            <button className='generate-btn'>Generate Innterview Report</button>
         </div>
+        
     </main>
   )
 }
